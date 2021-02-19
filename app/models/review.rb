85a90c5f0ae372044e6b176dd61baaebdad971ff
Tait Haslam <tait.haslam@gmail.com>
2021-02-18 20:42:12 -0500

    only 5 of these bastards left

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
+  validates :content, length: { minimum: 5 }
 end
